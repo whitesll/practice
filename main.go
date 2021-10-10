61368ad3a61f9d8568a60af479a306235b756521
whitesllgit <t-nisioka@hotmail.com>
2021-10-10 14:06:19 +0900

    index編集

--- main.go
+++ main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
-	router.LoadHTMLGlob("templates/*.html")
+	router.LoadHTMLGlob("views/*.html")
+	router.Static("/assets", "./assets")
 	data := "Hello Go/Gin!!"
 
 	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(200, "index.html", gin.H{"data": data})
+		ctx.HTML(http.StatusOK, "index.html", gin.H{"data": data})
 	})
 
-	router.Run()
+	router.Run(":8080")
 }
