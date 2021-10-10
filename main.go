e45ec1a5c9d3aa721d88661b6591c483fc71ffcb
whitesllgit <t-nisioka@hotmail.com>
2021-10-10 19:04:57 +0900

    練習

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"net/http"
+	"github.com/whitesll/plactice/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,11 +10,17 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("views/*.html")
 	router.Static("/assets", "./assets")
-	data := "Hello Go/Gin!!"
 
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "index.html", gin.H{"data": data})
-	})
+	user := router.Group("/user")
+	{
+		user.POST("/signup", routes.UserSignUp)
+		user.POST("/login", routes.UserLogIn)
+	}
+
+	router.GET("/", routes.Home)
+	router.GET("/login", routes.LogIn)
+	router.GET("/signup", routes.SignUp)
+	router.NoRoute(routes.NoRoute)
 
 	router.Run(":8080")
 }
